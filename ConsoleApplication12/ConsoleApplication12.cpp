230c8d73c10afb85f0948bc0342165780f5d63fb
Alexander Dm. <dabl21291@gmail.com>
2024-09-16 16:22:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a = 10;
+	cout << &a << endl; 
+	int* ptr = &a;
+	cout << ptr << endl;
+
+	cout << *ptr << endl; 
+	*ptr = 1500;
+	cout << a << endl; 
+}
+
